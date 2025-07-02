df13352cb7c7403c1c7edcc8f8ddf26cb705d9f0
XNandha <cakalika.fernandha@gmail.com>
2025-07-02 18:51:01 +0700

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -156,7 +156,7 @@ export default function LandingPage() {
         <section className="p-6">
           <h1 className="text-3xl font-bold">JMT Archery</h1>
           <p className="text-base md:text-lg mb-2">
-            Lorem ipsum dolor sit amet
+            Kebutuhan alat panahanmu
           </p>
           <button
             className="bg-black text-white px-6 py-2 rounded text-lg md:text-xl"
@@ -177,9 +177,6 @@ export default function LandingPage() {
               alt="Landing Banner"
               className="w-full h-full object-cover rounded"
             />
-            <span className="z-10 text-3xl font-bold text-white drop-shadow absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-              JMT Archery
-            </span>
           </div>
         </div>
 
